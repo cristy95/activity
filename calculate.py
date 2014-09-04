433ea7fb0f72ed599960e807218cca8d44c37cbf
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-04 22:04:39 +0800

    working page but without style

---
+++ calculate.py
@@ -0,0 +1,23 @@
+import cgi
+import json
+
+
+def index(req, number, raiseTo):
+    number = cgi.escape(number)
+    raiseTo = cgi.escape(raiseTo)
+    a = int(number)
+    e = int(raiseTo)
+    if raiseTo == 2:
+	square = make_exponentiater(e)
+	result = square(a)
+        ##print result
+    else:
+	cube = make_exponentiater(e)
+	result = cube(a)
+        ##print result
+	return json.dumps(result)
+
+def make_exponentiater(raiseTo):
+    return lambda(x): pow(x, raiseTo) 
+
+
--- globs.js
+++ globs.js
@@ -1,22 +1,24 @@
-var siteloc = "http:localhost";
+var siteloc = "http://localhost";
 var scriptloc = "/activity/"
 
 function calculate()
 {
   $.ajax({
-      url: siteloc + scriptloc + "activity.py",
+      url: siteloc + scriptloc + "calculate.py",
       data: {number:$("#number").val(),
              raiseTo:$("#raiseTo").val()},
       dataType: 'json',
       success: function (res) {
                   console.log(res);
-                  if(res[0][0] != "None")
+                  if(res != 0)
                   {
-			answer = "<div>";
-			answer = "<p>Answer = " + res[0][0] + "</p>" 
-			answer += "</div>";
+			answer = '<div class="table-responsive"">';
+			answer += '<table class"table table-condensed">';
+			answer += '<thead>'+'<tr>'+'<th>Answer</th></tr></thead>' + '<tbody>'+'<tr>'+'<th>'+res+'</th>'+'</tr>'+'</tbody>';
+			answer += '</table>'+'</div>';
 					  $("#target").html(answer); 
 				  } // end if
               }
     });
 }
+
